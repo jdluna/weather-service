e65a3c3445913752eb810a463e78c6762df8d9ac
Dario Luna <dario.luna@rappi.com>
2019-12-14 12:41:24 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `jdluna`.`weather` (`city`, `description`, `icon`) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
+INSERT INTO `jdluna`.`weather` (`city`, `description`, `icon`) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
